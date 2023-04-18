3308c0bea6df3c4756aacb3e4231afa55ce40f70
Nick Castle <castle5896@gmail.com>
2023-04-18 17:10:58 -0400

    Computed Properties

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
+      brand: 'NCastle Dev',
       product: 'Socks',
       description: 'The best socks you will find on the internet',
-      image: './assets/images/socks_green.jpg',
+      selectedVariant: 0,
       url: 'https://www.vuemastery.com/courses/intro-to-vue-3',
-      inStock: true,
       inventory: 7,
       onSale: false,
       details: ['50% cotton', '30% wool', '20% polyester'],
@@ -15,11 +15,13 @@ const app = Vue.createApp({
           id: 2234,
           color: 'green',
           image: './assets/images/socks_green.jpg',
+          quantity: 33,
         },
         {
           id: 2234,
           color: 'blue',
           image: './assets/images/socks_blue.jpg',
+          quantity: 0,
         },
       ],
       sizes: ['tiny timmy', 'regular randle', 'large and in charge larry'],
@@ -47,8 +49,30 @@ const app = Vue.createApp({
         this.inStock = false;
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      // let titleString = this.brand + ' ' + this.product;
+      // if (this.onSale) {
+      //   titleString += ' is on sale!';
+      // }
+      // return titleString;
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return ' is on sale!';
+      }
+      return '';
     },
   },
 });
